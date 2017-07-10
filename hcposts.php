3304e7fd241f7df5bbe470d664cef3b6fb3d54f5
Marius Nicula <mariusgnicula@gmail.com>
2017-07-10 21:35:53 +0300

    basic files

---
+++ hcposts.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: HC Posts
+Plugin URI: https://github.com/mariusgnicula/hcposts
+Description: A simple Wordpress Posts Plugin
+Version: 0.1
+Author: Marius Nicula
+Author URI: https://github.com/mariusgnicula
+License: GPL2
+*/
+
+define( 'HCP_PLUGIN', __FILE__ );
+
+define( 'HCP_PLUGIN_DIR', untrailingslashit( dirname( HCP_PLUGIN ) ) );
+
+require_once HCP_PLUGIN_DIR . '/settings.php';
---
+++ settings.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once HCP_PLUGIN_DIR . '/includes/hc_cpt.php';
+require_once HCP_PLUGIN_DIR . '/includes/hc_settings_page.php';
+require_once HCP_PLUGIN_DIR . '/includes/hc_functions.php';
